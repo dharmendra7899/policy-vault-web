1b7a5581d358976d474248fc8defa303dea3b3e5
dharmendra7899 <devskyview7899@gmail.com>
2025-08-07 11:53:47 +0530

    Build: Policy Vault web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3fe694a184174bebe84b0154d4121d38",
+const RESOURCES = {"flutter_bootstrap.js": "3c9de9b6dedba3e6f7436da7138e8b9c",
 "version.json": "ed6b5cb6abf4b58f2ce9c857051534bf",
 "index.html": "6c59c00d11d2135840b6eea0d4dbe888",
 "/": "6c59c00d11d2135840b6eea0d4dbe888",
